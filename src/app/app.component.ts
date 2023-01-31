f13c001fed61095739f2d039fa6142383963328c
Karthikeyan Pandian <karthikeyanpandian92@gmail.com>
2023-01-31 20:42:10 +0530

    Adding second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ColDef } from 'ag-grid-community';
+import { ColDef, GridApi, GridReadyEvent } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
@@ -7,27 +7,63 @@ import { ColDef } from 'ag-grid-community';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+
+  private gridApi!: GridApi;
+  private gridApi2!: GridApi;
+
   title = 'selectionApp';
 
-  columnDefs: ColDef[] = [
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price' },
+  onGridReady(params: GridReadyEvent) {    
+    this.gridApi = params.api;
+  }
+
+  onGridReady2(params: GridReadyEvent) {    
+    this.gridApi2 = params.api;
+  }
+
+  onFilterTextBoxChanged(evt: any) {
+    console.log(evt.target.value);
+    this.gridApi.setQuickFilter(evt.target.value);   
+  }
+
+  onFilterTextBoxChanged2(evt: any) {
+    console.log(evt.target.value);
+    this.gridApi2.setQuickFilter(evt.target.value);   
+  }
+
+  defaultColDef = {
+    resizable: true
+  };
+
+  columnDefs: any[] = [
+    {
+      headerName: '',
+      width: 40,
+      checkboxSelection: true,
+      suppressMenu: true,
+      pinned: true,
+      headerCheckboxSelection: true,
+      headerCheckboxSelectionFilteredOnly: true,
+      suppressSorting: true
+    },
+    { field: 'bnyAcctNo', headerName: 'BNYM Account Number', width: 200 },
+    { field: 'nochuFundNo', headerName: 'Nochu Account Number', width: 200 },
+    { field: 'acctName', headerName: 'Account Name', width: 150 }
   ];
 
   rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
+    { bnyAcctNo: 'Toyota', nochuFundNo: 'Celica', acctName: 35000 },
+    { bnyAcctNo: 'Ford', nochuFundNo: 'Mondeo', acctName: 32000 },
+    { bnyAcctNo: 'Porsche', nochuFundNo: 'Boxster', acctName: 72000 },
+    { bnyAcctNo: 'Toyota', nochuFundNo: 'Celica', acctName: 35000 },
+    { bnyAcctNo: 'Ford', nochuFundNo: 'Mondeo', acctName: 32000 },
+    { bnyAcctNo: 'Porsche', nochuFundNo: 'Boxster', acctName: 72000 },
+    { bnyAcctNo: 'Toyota', nochuFundNo: 'Celica', acctName: 35000 },
+    { bnyAcctNo: 'Ford', nochuFundNo: 'Mondeo', acctName: 32000 },
+    { bnyAcctNo: 'Porsche', nochuFundNo: 'Boxster', acctName: 72000 },
   ];
 
-  buttonClick(){
-    console.log("Button Clicked!");
+  buttonClick() {
+    console.log('Button Clicked!');
   }
 }
